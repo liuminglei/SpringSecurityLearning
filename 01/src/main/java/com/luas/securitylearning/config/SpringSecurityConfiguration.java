f422e01f8c00d733965ce8e06ca47851a7aee302
liuminglei <812934656@qq.com>
2020-06-16 10:07:57 +0800

    删除冗余代码

--- 01/src/main/java/com/luas/securitylearning/config/SpringSecurityConfiguration.java
+++ 01/src/main/java/com/luas/securitylearning/config/SpringSecurityConfiguration.java
@@ -1,7 +1,6 @@
 package com.luas.securitylearning.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -9,13 +8,4 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 public class SpringSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-            .formLogin()
-            .and()
-            .authorizeRequests()
-            .anyRequest()
-            .authenticated();
-    }
 }
