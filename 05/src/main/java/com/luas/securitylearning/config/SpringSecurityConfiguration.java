f110f36a902b07f1fafe4f993e203405e03c2255
liuminglei <812934656@qq.com>
2020-06-19 16:50:37 +0800

    05-成功登录SuccessHandler高级用法

---
+++ 05/src/main/java/com/luas/securitylearning/config/SpringSecurityConfiguration.java
@@ -0,0 +1,56 @@
+package com.luas.securitylearning.config;
+
+import com.luas.securitylearning.security.web.authentication.CustomSavedRequestAwareAuthenticationSuccessHandler;
+import com.luas.securitylearning.service.EmailService;
+import com.luas.securitylearning.service.SmsService;
+import com.luas.securitylearning.service.WeChatService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+@EnableWebSecurity
+@Configuration
+public class SpringSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    private EmailService emailService;
+
+    private SmsService smsService;
+
+    private WeChatService wechatService;
+
+    public SpringSecurityConfiguration(EmailService emailService, SmsService smsService, WeChatService wechatService) {
+        this.emailService = emailService;
+        this.smsService = smsService;
+        this.wechatService = wechatService;
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/css/**", "/js/**", "/plugins/**", "/images/**", "/fonts/**");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .formLogin().loginPage("/login").successHandler(customAuthenticationSuccessHandler()).permitAll()
+                .and()
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated()
+        ;
+    }
+
+    @Bean
+    public AuthenticationSuccessHandler customAuthenticationSuccessHandler() {
+        CustomSavedRequestAwareAuthenticationSuccessHandler customSavedRequestAwareAuthenticationSuccessHandler = new CustomSavedRequestAwareAuthenticationSuccessHandler();
+        customSavedRequestAwareAuthenticationSuccessHandler.setEmailService(emailService);
+        customSavedRequestAwareAuthenticationSuccessHandler.setSmsService(smsService);
+        customSavedRequestAwareAuthenticationSuccessHandler.setWeChatService(wechatService);
+        return customSavedRequestAwareAuthenticationSuccessHandler;
+    }
+
+}
