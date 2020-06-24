99459189fff616336cd8106ab10debcc49e63224
liuminglei <812934656@qq.com>
2020-06-24 10:04:52 +0800

    09-自定义用户登录失败页面

---
+++ 09/src/main/java/com/luas/securitylearning/config/SpringSecurityConfiguration.java
@@ -0,0 +1,39 @@
+package com.luas.securitylearning.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+public class SpringSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/css/**", "/js/**", "/plugins/**", "/images/**", "/fonts/**");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .formLogin()
+                .loginPage("/login")
+                .defaultSuccessUrl("/index")
+                .failureUrl("/login_fail")
+                .permitAll()
+                .and()
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .logout()
+                .logoutSuccessUrl("/logout_success")
+                .permitAll()
+                .and()
+                .csrf().disable()
+        ;
+    }
+
+}
