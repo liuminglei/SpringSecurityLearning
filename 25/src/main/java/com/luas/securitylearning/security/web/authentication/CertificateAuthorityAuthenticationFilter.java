f31c61ed7544e84807b1f99322011773cde8094a
liuminglei <812934656@qq.com>
2020-08-21 17:20:25 +0800

    新增CertificateAuthorityAuthenticationFilter

---
+++ 25/src/main/java/com/luas/securitylearning/security/web/authentication/CertificateAuthorityAuthenticationFilter.java
@@ -0,0 +1,152 @@
+package com.luas.securitylearning.security.web.authentication;
+
+import com.luas.securitylearning.security.authentication.CertificateAuthorityAuthenticationToken;
+import org.springframework.security.authentication.AuthenticationServiceException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.util.Assert;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class CertificateAuthorityAuthenticationFilter extends AbstractAuthenticationProcessingFilter {
+
+    // ~ Static fields/initializers
+    // =====================================================================================
+
+    public static final String SPRING_SECURITY_FORM_SIGNATURE_KEY = "signature";
+    public static final String SPRING_SECURITY_FORM_PASSWORD_KEY = "password";
+
+    private String signatureParameter = SPRING_SECURITY_FORM_SIGNATURE_KEY;
+    private String passwordParameter = SPRING_SECURITY_FORM_PASSWORD_KEY;
+    private boolean postOnly = true;
+
+    // ~ Constructors
+    // ===================================================================================================
+
+    public CertificateAuthorityAuthenticationFilter() {
+        super(new AntPathRequestMatcher("/certificate_authority_login", "POST"));
+    }
+
+    // ~ Methods
+    // ========================================================================================================
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        if (postOnly && !request.getMethod().equals("POST")) {
+            throw new AuthenticationServiceException(
+                    "Authentication method not supported: " + request.getMethod());
+        }
+
+        String signature = obtainSignature(request);
+        String password = obtainPassword(request);
+
+        if (signature == null) {
+            signature = "";
+        }
+
+        if (password == null) {
+            password = "";
+        }
+
+        signature = signature.trim();
+
+        CertificateAuthorityAuthenticationToken authRequest = new CertificateAuthorityAuthenticationToken(
+                signature, password);
+
+        // Allow subclasses to set the "details" property
+        setDetails(request, authRequest);
+
+        return this.getAuthenticationManager().authenticate(authRequest);
+    }
+
+    /**
+     * Enables subclasses to override the composition of the password, such as by
+     * including additional values and a separator.
+     * <p>
+     * This might be used for example if a postcode/zipcode was required in addition to
+     * the password. A delimiter such as a pipe (|) should be used to separate the
+     * password and extended value(s). The <code>AuthenticationDao</code> will need to
+     * generate the expected password in a corresponding manner.
+     * </p>
+     *
+     * @param request so that request attributes can be retrieved
+     *
+     * @return the password that will be presented in the <code>Authentication</code>
+     * request token to the <code>AuthenticationManager</code>
+     */
+    protected String obtainPassword(HttpServletRequest request) {
+        return request.getParameter(passwordParameter);
+    }
+
+    /**
+     * Enables subclasses to override the composition of the signature, such as by
+     * including additional values and a separator.
+     *
+     * @param request so that request attributes can be retrieved
+     *
+     * @return the signature that will be presented in the <code>Authentication</code>
+     * request token to the <code>AuthenticationManager</code>
+     */
+    protected String obtainSignature(HttpServletRequest request) {
+        return request.getParameter(signatureParameter);
+    }
+
+    /**
+     * Provided so that subclasses may configure what is put into the authentication
+     * request's details property.
+     *
+     * @param request that an authentication request is being created for
+     * @param authRequest the authentication request object that should have its details
+     * set
+     */
+    protected void setDetails(HttpServletRequest request,
+                              CertificateAuthorityAuthenticationToken authRequest) {
+        authRequest.setDetails(authenticationDetailsSource.buildDetails(request));
+    }
+
+    /**
+     * Sets the parameter name which will be used to obtain the signature from the login
+     * request.
+     *
+     * @param signatureParameter the parameter name. Defaults to "signature".
+     */
+    public void setSignatureParameter(String signatureParameter) {
+        Assert.hasText(signatureParameter, "Signature parameter must not be empty or null");
+        this.signatureParameter = signatureParameter;
+    }
+
+    /**
+     * Sets the parameter name which will be used to obtain the password from the login
+     * request..
+     *
+     * @param passwordParameter the parameter name. Defaults to "password".
+     */
+    public void setPasswordParameter(String passwordParameter) {
+        Assert.hasText(passwordParameter, "Password parameter must not be empty or null");
+        this.passwordParameter = passwordParameter;
+    }
+
+    /**
+     * Defines whether only HTTP POST requests will be allowed by this filter. If set to
+     * true, and an authentication request is received which is not a POST request, an
+     * exception will be raised immediately and authentication will not be attempted. The
+     * <tt>unsuccessfulAuthentication()</tt> method will be called as if handling a failed
+     * authentication.
+     * <p>
+     * Defaults to <tt>true</tt> but may be overridden by subclasses.
+     */
+    public void setPostOnly(boolean postOnly) {
+        this.postOnly = postOnly;
+    }
+
+    public final String getSignatureParameter() {
+        return signatureParameter;
+    }
+
+    public final String getPasswordParameter() {
+        return passwordParameter;
+    }
+}
