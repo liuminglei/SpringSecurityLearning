e4a657cdb3c2475efb291fd48a276134cbb125a9
liuminglei <liuminglei200@163.com>
2020-09-11 17:42:13 +0800

    27-AuthenticationManager默认实现之ProviderManager详解

---
+++ 27/src/main/java/com/luas/securitylearning/security/core/userdetails/CertificateAuthorityDaoAuthenticationProvider.java
@@ -0,0 +1,67 @@
+package com.luas.securitylearning.security.core.userdetails;
+
+import com.luas.securitylearning.security.authentication.CertificateAuthorityAuthenticationToken;
+
+import org.springframework.security.authentication.InternalAuthenticationServiceException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.authentication.dao.AbstractUserDetailsAuthenticationProvider;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.util.Assert;
+
+public class CertificateAuthorityDaoAuthenticationProvider extends AbstractUserDetailsAuthenticationProvider {
+
+    private UserDetailsService userDetailsService;
+
+    public CertificateAuthorityDaoAuthenticationProvider() {
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return (CertificateAuthorityAuthenticationToken.class.isAssignableFrom(authentication));
+    }
+
+    @SuppressWarnings("deprecation")
+    protected void additionalAuthenticationChecks(UserDetails userDetails,
+                                                  UsernamePasswordAuthenticationToken authentication)
+            throws AuthenticationException {
+        // do nothing.
+    }
+
+    protected void doAfterPropertiesSet() throws Exception {
+        Assert.notNull(this.userDetailsService, "A UserDetailsService must be set");
+    }
+
+    protected final UserDetails retrieveUser(String username,
+                                             UsernamePasswordAuthenticationToken authentication)
+            throws AuthenticationException {
+        try {
+            UserDetails loadedUser = this.getUserDetailsService().loadUserByUsername(username);
+            if (loadedUser == null) {
+                throw new InternalAuthenticationServiceException(
+                        "UserDetailsService returned null, which is an interface contract violation");
+            }
+            return loadedUser;
+        }
+        catch (UsernameNotFoundException ex) {
+            throw ex;
+        }
+        catch (InternalAuthenticationServiceException ex) {
+            throw ex;
+        }
+        catch (Exception ex) {
+            throw new InternalAuthenticationServiceException(ex.getMessage(), ex);
+        }
+    }
+
+    public void setUserDetailsService(UserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
+    protected UserDetailsService getUserDetailsService() {
+        return userDetailsService;
+    }
+
+}
